9f6d4dba4748116788a8d80de84417bb554099af
Vladyslav Vagin <vladtwince@gmail.com>
2025-08-02 19:05:09 +0200

    update

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
+
+export const geistInter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL)
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistInter.className} antialiased`}
+      >
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen">
+            <h1 className="text-4xl font-bold">404 - Not Found</h1>
+        </div>
+    );
+}
+ 
+export default NotFoundPage;
